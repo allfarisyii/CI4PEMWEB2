0e09a68eb6910735115c555767dcd63a34c242cb
allfarisyii <muhammadalfarisyi@saintek.unipdu.ac.id>
2024-03-30 08:14:37 +0700

    create model in ci

--- ci4composer/app/Config/Routes.php
+++ ci4composer/app/Config/Routes.php
@@ -5,11 +5,4 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-
-$routes->get('/', 'Home::index');
-
-$routes->get('/home', 'Pages::home');
-$routes->get('/about', 'Pages::about');
-$routes->get('/contact', 'pages::contact');
-
-$routes->setAutoRoute(true);
+$routes->get('/', 'Pages::index');
